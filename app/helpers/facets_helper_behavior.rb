5c345f38dd3a80f764a3710a11bf8d291521ef4a
Mike Korcynski <mkorcy@gmail.com>
2013-07-03 15:32:45 -0400

    updated render_facet_value to its correct location in new blacklight so facets render on one line

---
+++ app/helpers/facets_helper_behavior.rb
@@ -0,0 +1,27 @@
+module Blacklight::FacetsHelperBehavior
+  include Blacklight::FacetsHelperBehavior
+
+
+  # Removing the [remove] link from the default selected facet display
+
+  def render_facet_value(facet_solr_field, item, options ={})
+    if item.is_a? Array
+      link_to_unless(options[:suppress_link], item[0], add_facet_params_and_redirect(facet_solr_field, item[0]), :class => "facet_select") + raw("<span class='numbercount'> (" + format_num(item[1]) + ")</span>")
+    else
+      text_for_link = item.value
+      unless options[:suppress_link]
+        #link_to_unless(, , :class=>"facet_select") do
+        link_to add_facet_params_and_redirect(facet_solr_field, item.value), :class => "facet_select" do
+          out = text_for_link
+
+          out +="<span class='numbercount'> ("+ format_num(item.hits) +")</span>"
+          out.html_safe
+        end
+      else
+        out = item.value
+        out.html_safe
+      end
+
+    end
+  end
+end
