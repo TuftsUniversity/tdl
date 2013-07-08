4336ec74867e77d65d9c1b3d9bb6b8464027dad9
Mike Korcynski <mkorcy@gmail.com>
2013-07-08 16:15:56 -0400

    added Capybara and some capybara configs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'rspec/autorun'
+require 'capybara/rspec'
+require 'test_helpers'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -35,4 +37,10 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+end
+
+Capybara.configure do |config|
+    config.app_host    = 'http://localhost'
+    config.server_port = 3000
+    config.always_include_port = true
 end
