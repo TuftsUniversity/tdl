4c064663063f11cadffc8bbfa89529d5dd282a74
WillHumphries <will.d.humphries@gmail.com>
2013-07-08 11:41:20 -0400

    Updated reference to blacklight config to be controller specific

--- app/helpers/blacklight_helper.rb
+++ app/helpers/blacklight_helper.rb
@@ -3,8 +3,8 @@ module BlacklightHelper
 
   # currently only used by the render_document_partial helper method (below)
   def document_partial_name(document)
-    return if document[Blacklight.config[:show][:display_type]].nil?
-    ModelNameHelper.map_model_name(document[Blacklight.config[:show][:display_type]].first).gsub(/^[^\/]+\/[^:]+:/, "").underscore.pluralize
+    return if document[CatalogController.blacklight_config.show.display_type].nil?
+    ModelNameHelper.map_model_name(document[CatalogController.blacklight_config.show.display_type].first).gsub(/^[^\/]+\/[^:]+:/, "").underscore.pluralize
   end
 
 
