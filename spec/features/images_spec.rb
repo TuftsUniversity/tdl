46a3a4580bc33fbced57ed72b3cd7ab01b0ad748
Mike Korcynski <mkorcy@gmail.com>
2013-07-08 16:19:28 -0400

    fixed include and contact link

--- spec/features/images_spec.rb
+++ spec/features/images_spec.rb
@@ -1,5 +1,6 @@
 require "spec_helper"
 feature  'images that should load but do not, in part because some static pages load images out of the dl, something to keep an eye on.' do
+  include TestHelpers
 
   scenario 'Walter B. Wriston collection image loads' do
       visit root_path
@@ -15,7 +16,7 @@ feature  'images that should load but do not, in part because some static pages 
 
   scenario 'contact page image loads' do
       visit root_path
-      click_link "CONTACT"
+      click_link "Contact"
       active_link?("#{Capybara.app_host}:#{Capybara.server_port.to_s}/file_assets/tufts:UA009.011.029.00009").should be
    end
 end
