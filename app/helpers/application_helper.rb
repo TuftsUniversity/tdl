c584cb20ce2787b72cd200f508f4377106335c2c
WillHumphries <will.d.humphries@gmail.com>
2013-08-14 14:34:37 -0400

    Moved faculty publications partial to new location, added some methods that support the book viewer to the application helper (from https://github.com/mkorcy/tdl_hydra_head), removed .text from OM metadata in a view template

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,11 @@ include Tufts::MetadataMethods
 
 module ApplicationHelper
 
+  def showPdfImage(pid)
+    result = "<img alt=\"\" src=\"/pdf_pages/" + pid + "/0\"/>"
+
+    return raw(result)
+  end
 
   def showImage(pid)
       result = "<img alt=\"\" src=\"" + file_asset_path(pid) + "\"/>"
@@ -16,6 +21,15 @@ module ApplicationHelper
 
   def render_image_viewer_link(pid)
     result = "<a href=\"" + render_image_viewer_path(pid) + "\"><h6>open in viewer <i class=\"icon-share\"></i></h6></a>"
+    return raw(result)
+  end
+
+  def render_book_viewer_path(pid)
+    "/bookreader/" + pid +"#page/1/mode/2up"
+  end
+
+  def render_book_viewer_link(pid)
+    result = "<a href=\"" + render_book_viewer_path(pid) + "\"><h6>open in viewer <i class=\"icon-share\"></i></h6></a>"
     return raw(result)
   end
 
