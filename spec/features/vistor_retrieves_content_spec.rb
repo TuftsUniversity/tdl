12fc298e5c19443979eb194c2a565e6b62d9a7f4
WillHumphries <will.d.humphries@gmail.com>
2013-08-27 13:42:13 -0400

    Expanded ead test to include eadinternal view

--- spec/features/vistor_retrieves_content_spec.rb
+++ spec/features/vistor_retrieves_content_spec.rb
@@ -52,6 +52,8 @@ feature 'Visitor goes directly to a catalog pid' do
     click_link 'View Finding Aid'
     page.should have_content "Russell Miller in his research for Light on the Hill"
     page.should have_link "General subject files, 1923-96 1923-96"
+    click_link 'General subject files, 1923-96 1923-96'
+    page.should have_content "Clippings, 1948-81"
 end
 
   scenario 'user loads Here and There at Tufts' do
