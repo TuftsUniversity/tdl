96fd3647063c81cbb7c30c6e792eaf8d767e76f4
WillHumphries <will.d.humphries@gmail.com>
2013-09-27 14:22:32 -0400

    added test for advanced search by title field, and title solr field config that makes it pass

--- spec/features/vistor_retrieves_search_results_spec.rb
+++ spec/features/vistor_retrieves_search_results_spec.rb
@@ -10,4 +10,17 @@ feature 'Visitor goes to homepage and does a search' do
       page.should have_content "3 Results"
   end
 
+  scenario 'user searches for title advanced' do
+      visit root_path
+      click_link 'Advanced Search'
+      within("div#advanced_search") do
+        find_field('title', :type=> 'text').set('ciampa')
+        find_field('title', :type=> 'text').value.should eq 'ciampa'
+      end
+      click_button 'advanced_search'
+      page.should have_content "1 Result"
+      page.should have_content "You searched for:"
+      page.should have_content "Adrienne Effron"
+  end
+
 end
