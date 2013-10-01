8dbf633d9474d142137100f4339bd0a3fd1a646b
WillHumphries <will.d.humphries@gmail.com>
2013-10-01 09:24:26 -0400

    comment out references to nonexistent spellcheck dictionaries

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -186,7 +186,7 @@ class CatalogController < ApplicationController
     
     config.add_search_field('title') do |field|
       # solr_parameters hash are sent to Solr as ordinary url query params. 
-      field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
+      # field.solr_parameters = { :'spellcheck.dictionary' => 'title' }
 
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
@@ -199,7 +199,7 @@ class CatalogController < ApplicationController
     end
     
     config.add_search_field('author') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
+      #field.solr_parameters = { :'spellcheck.dictionary' => 'author' }
       field.label = 'Creator/Author'
       field.solr_local_parameters = { 
         :qf => '$author_qf',
@@ -217,7 +217,7 @@ class CatalogController < ApplicationController
 
     config.add_search_field('collection') do |field|
       field.include_in_simple_select = false
-      field.solr_parameters = { :'spellcheck.dictionary' => 'collection' }
+      #field.solr_parameters = { :'spellcheck.dictionary' => 'collection' }
       field.solr_local_parameters = {
         :qf => '$collection_qf',
         :pf => '$collection_pf'
