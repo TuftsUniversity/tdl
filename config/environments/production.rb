f4a8df3cdc25ece02d50aadbe5f927327f9499dc
Mike Korcynski <mkorcy@gmail.com>
2013-12-23 11:22:25 -0500

    fixed the rest of the tei rendering

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ TDL::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( tdl.css tdl.js search_controls.js tuftsification.js tuftsification.css jwplayer.flash.swf )
+  config.assets.precompile += %w( tuftsification.js tuftsification.css tei.js pdfviewer/BookReaderPDFViewer.js BookReader.css tei/tei.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
