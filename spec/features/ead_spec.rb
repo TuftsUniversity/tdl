65c72719daa2064391f7d73fdcfb0b4c3bd71c98
Mike Korcynski <mkorcy@gmail.com>
2014-10-22 17:29:02 -0400

    adds another similar test for link within finding aid

--- spec/features/ead_spec.rb
+++ spec/features/ead_spec.rb
@@ -5,9 +5,17 @@ feature 'EAD' do
   before do
     @ead = TuftsEAD.find('tufts:UA069.001.DO.UA015')
   end
-  
+
   scenario 'View Online Materials should link to associated materials in search results' do
     visit catalog_path(@ead)
+    click_link "View Online Materials", :exact => false
+    page.should have_text "Alliance for Progress or Alianza Para El Progreso? A Reassessment of the Latin American Contribution to the Alliance for Progress"
+
+  end
+
+  scenario 'from the Finding Aid Viewer, View Online Materials should link to associated materials in search results' do
+    visit catalog_path(@ead)
+    click_link "View Finding Aid", :exact => false
     click_link "View Online Materials", :exact => false
     page.should have_text "Alliance for Progress or Alianza Para El Progreso? A Reassessment of the Latin American Contribution to the Alliance for Progress"
 
