c8eb9ccbfaf08e116bf254d7607d7c3dee3fef5a
Mike Korcynski <mkorcy@gmail.com>
2015-10-09 11:59:00 -0400

    adds seeds for test user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Begin loading seed data"
+
+puts "Loading Roles:"
+roles = ["admin","digital_repository_admin"]
+
+roles.each do | name |
+  puts "  #{name}"
+  Role.first_or_create(name: name)
+end
+
+users = ["aa729"]
+
+roles.each do | name |
+  puts "  #{name}"
+  User.first_or_create(name: name)
+end
+
+ActiveRecord::Base.connection.execute "insert into roles_users values (2, 1)"
+
+puts "Data seeding finished"
