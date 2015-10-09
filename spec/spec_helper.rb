7def2ccd4f600c9c5509c38718c210d944e35c42
Mike Korcynski <mkorcy@gmail.com>
2015-10-09 11:59:14 -0400

    uses ladle for ldap testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,6 @@
 require 'simplecov'
 require 'simplecov-rcov'
+require 'ladle'
 SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start
 
@@ -23,6 +24,9 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  config.before(:suite) do
+    @ldap_server = Ladle::Server.new(:quiet => false, :ldif => File.expand_path('../fixtures/tufts_ldap.ldif', __FILE__)).start
+  end
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
