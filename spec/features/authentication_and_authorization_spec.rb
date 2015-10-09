dbf8c9ac96e27b60c5d5814ce4503d64840d7ed5
Mike Korcynski <mkorcy@gmail.com>
2015-10-09 13:29:02 -0400

    still trying to get the auth specs right

--- spec/features/authentication_and_authorization_spec.rb
+++ spec/features/authentication_and_authorization_spec.rb
@@ -11,7 +11,7 @@ feature 'Visitor can login with correct username and password and role and is ot
     click_link 'Staff Login'
     page.should have_content "Tufts Username"
     fill_in 'user_username', :with=>'aa729'
-    fill_in 'user_username', :with=>'password'
+    fill_in 'user_password', :with=>'password'
     click_button 'Log In'
     page.should have_content "Invalid username or password."
   end
@@ -22,7 +22,7 @@ feature 'Visitor can login with correct username and password and role and is ot
     click_link 'Staff Login'
     page.should have_content "Tufts Username"
     fill_in 'user_username', :with=>'aa729'
-    fill_in 'user_username', :with=>'smada'
+    fill_in 'user_password', :with=>'smada'
     click_button 'Log In'
     page.should have_content "Signed in successfully."
   end
@@ -33,9 +33,9 @@ feature 'Visitor can login with correct username and password and role and is ot
     click_link 'Staff Login'
     page.should have_content "Tufts Username"
     fill_in 'user_username', :with=>'bb459'
-    fill_in 'user_username', :with=>'niwdlab'
+    fill_in 'user_password', :with=>'niwdlab'
     click_button 'Log In'
-    page.should have_content "Invalid username or password."
+    page.should have_content "Signed in successfully."
   end
 
   scenario 'a known user with invalid role is rejected with incorrect ldap password' do
@@ -44,7 +44,7 @@ feature 'Visitor can login with correct username and password and role and is ot
     click_link 'Staff Login'
     page.should have_content "Tufts Username"
     fill_in 'user_username', :with=>'bb459'
-    fill_in 'user_username', :with=>'niwdlabNOT'
+    fill_in 'user_password', :with=>'niwdlabNOT'
     click_button 'Log In'
     page.should have_content "Invalid username or password."
   end
