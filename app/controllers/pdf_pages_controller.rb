0cb609d68491315c09495b38cf20bf8d16e41ccb
Mike Korcynski <mkorcy@gmail.com>
2016-06-10 16:13:44 -0400

    adds check for access permissions

--- app/controllers/pdf_pages_controller.rb
+++ app/controllers/pdf_pages_controller.rb
@@ -20,7 +20,7 @@ class PdfPagesController < CatalogController
         @downloadable = true
       end
 
-     # mapped_model_names = ModelNameHelper.map_model_names(@file_asset.relationships(:has_model))
+      # mapped_model_names = ModelNameHelper.map_model_names(@file_asset.relationships(:has_model))
       # pdf_pages = Settings.pdfpages.pagelocation
       #file name format PB.002.001.00001-0.png
       # pid-pagenumber.png
@@ -32,14 +32,17 @@ class PdfPagesController < CatalogController
   end
 
   def show
-    @file_asset = FileAsset.find(params[:id])
+    @file_asset = TuftsBase.find(params[:id])
     if (@file_asset.nil?)
       logger.warn("No such file asset: " + params[:id])
       flash[:retrieval]= "No such file asset."
       redirect_to(:action => 'index', :q => nil, :f => nil)
     else
+      if isUnderEmbargo || isMissingCommunityMemberRole
+        redirect_to(:root, :q => nil, :f => nil) and return false
+      end
       # get containing object for this FileAsset
-      pid = @file_asset.container_id
+      pid = @file_asset.pid
       @downloadable = false
       # A FileAsset is downloadable iff the user has read or higher access to a parent
       @response, @permissions_solr_document = get_solr_response_for_doc_id(pid)
@@ -54,8 +57,34 @@ class PdfPagesController < CatalogController
       # /pdf_pages/data05/tufts/central/dca/PB/access_pdf_pageimages/PB.002.001.00001
       dsLocation = @file_asset.datastreams["Archival.pdf"].dsLocation
       local_path = convert_url_to_png_path(dsLocation, params[:pageNumber], params[:id])
-      send_file(local_path);
+
+      send_file(local_path)
     end
+  end
+
+  private
+
+  def isMissingCommunityMemberRole
+
+    return unless @file_asset.datastreams["DCA-ADMIN"].visibility.include? "authenticated"
+
+    if current_user.nil?
+      return true
+    end
+
+    if !current_user.nil? and !current_user.has_role? :community_member
+      return true
+    end
+
+  end
+
+  def isUnderEmbargo
+    if @file_asset.datastreams["DCA-ADMIN"].under_embargo?
+      logger.warn("File asset embargoed: " + params[:id])
+      flash[:retrieval]= "File asset embargoed."
+      return true
+    end
+
   end
 
 end
